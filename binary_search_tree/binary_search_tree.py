0ea9f85e96b7b9eb1d85a66e09bf90374c403b69
jacobpad <jacobpad@gmail.com>
2020-07-15 16:03:52 -0700

    Binary search tree 'get_max'

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,33 +9,36 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
-    # Insert the given value into the tree
     def insert(self, value):
-                                                # Left side
+    # Insert the given value into the tree
+        # Left side
         if value < self.value:                  # If the value is on the left side is less than the root node
             if self.left is None:               # If it's empty, put it here
                 self.left = BSTNode(value)      # Insert a node to this spot
-            else:                               # 
+            else:                               #
                 self.left.insert(value)         # Recursive - calling itself
 
         elif value >= self.value:               # Right side
             if self.right is None:              # If there is no value to the right, insert it here by
-                self.right = BSTNode(value)     # assigning it to the right node.
+                # assigning it to the right node.
+                self.right = BSTNode(value)
             else:                               # If it doesn't go down that right side,
                 self.right.insert(value)        # run it again.
 
+    def contains(self, target):
     # Return True if the tree contains the value
     # False if it does not
-    def contains(self, target):
         if self.value == target:                # If what we're looking for is the same as root
             return True
-        
+
         if target < self.value:                 # If target is less than self.value
             if self.left is None:
                 return False
@@ -47,15 +50,23 @@ class BSTNode:
                 return False
             else:
                 return self.right.contains(target)
-        
-        
 
-    # Return the maximum value found in the tree
+
     def get_max(self):
-        pass
+    # Return the maximum value found in the tree
+        # If there's only one node
+        # Only deal with the right side as smaller values always go left
+        current = self.right                    # Set the right node as the current node
+        value = self.value                      # Set self.value to value
+        while current != None:                  # If there's a node to the right,
+            value = current.value               # update value to the current node &
+            # set the node to the right as the current node.
+            current = current.right
+        return value
 
-    # Call the function `fn` on the value of each node
+
     def for_each(self, fn):
+    # Call the function `fn` on the value of each node
         pass
 
     # Part 2 -----------------------
